f09e462e16ae52a6f0ca0d024d6599b3de0c349f
risunyaaa <risunyaaa@yandex.ru>
2024-01-30 16:41:52 +0300

    version 5.0.0 / add eslint and prettier

--- main.js
+++ main.js
@@ -1,29 +1,27 @@
 //  скрипт для получения cv
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Получаем все элементы с классом "cv-button"
-    const downloadButtons = document.querySelectorAll('.cv-button');
+document.addEventListener("DOMContentLoaded", function () {
+  // Получаем все элементы с классом "cv-button"
+  const downloadButtons = document.querySelectorAll(".cv-button");
 
-    // Добавляем обработчик события на клик по каждой кнопке
-    downloadButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            // Создаем ссылку для скачивания файла
-            const downloadLink = document.createElement('a');
-            downloadLink.href = '/images/resume.pdf'; // Укажите здесь путь к файлу для скачивания
-            downloadLink.download = 'AleshkinCV'; // Укажите здесь название файла при скачивании
+  // Добавляем обработчик события на клик по каждой кнопке
+  downloadButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      // Создаем ссылку для скачивания файла
+      const downloadLink = document.createElement("a");
+      downloadLink.href = "/images/resume.pdf"; // Укажите здесь путь к файлу для скачивания
+      downloadLink.download = "AleshkinCV"; // Укажите здесь название файла при скачивании
 
-            // Добавляем ссылку на страницу
-            document.body.appendChild(downloadLink);
+      // Добавляем ссылку на страницу
+      document.body.appendChild(downloadLink);
 
-            // Эмулируем клик по ссылке для начала скачивания файла
-            downloadLink.click();
+      // Эмулируем клик по ссылке для начала скачивания файла
+      downloadLink.click();
 
-            // Удаляем ссылку после скачивания файла
-            document.body.removeChild(downloadLink);
-        });
+      // Удаляем ссылку после скачивания файла
+      document.body.removeChild(downloadLink);
     });
+  });
 });
 
-// скрипт для отправки сообщения 
-
-
+// скрипт для отправки сообщения
