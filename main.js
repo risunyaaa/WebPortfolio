77cf4b5ff85de0d8760bded5235b23077d86aa95
risunyaaa <risunyaaa@yandex.ru>
2024-03-20 21:47:04 +0300

    v 13.0
    add hamburger menu

--- main.js
+++ main.js
@@ -24,3 +24,12 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+
+const hamMenu = document.querySelector('.ham-menu');
+
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+hamMenu.classList.toggle('active');
+offScreenMenu.classList.toggle('active');
+})   
